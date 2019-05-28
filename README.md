b81e6d1552c46abf65955f2e2371057645e368a3
mathieu.brunot <mb.mathieu.brunot@gmail.com>
2019-05-28 20:21:07 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+
+[uri_license]: http://www.gnu.org/licenses/agpl.html
+[uri_license_image]: https://img.shields.io/badge/License-AGPL%20v3-blue.svg
+
+[![License: AGPL v3][uri_license_image]][uri_license]
+[![Build Status](https://travis-ci.org/Monogramm/docker-frappe.svg)](https://travis-ci.org/Monogramm/docker-frappe)
+[![Docker Automated buid](https://img.shields.io/docker/build/monogramm/docker-frappe.svg)](https://hub.docker.com/r/monogramm/docker-frappe/)
+[![Docker Pulls](https://img.shields.io/docker/pulls/monogramm/docker-frappe.svg)](https://hub.docker.com/r/monogramm/docker-frappe/)
+[![](https://images.microbadger.com/badges/version/monogramm/docker-frappe.svg)](https://microbadger.com/images/monogramm/docker-frappe)
+[![](https://images.microbadger.com/badges/image/monogramm/docker-frappe.svg)](https://microbadger.com/images/monogramm/docker-frappe)
+
+# Frappe custom Docker container
+
+Docker image for Frappe applications.
+
+This image is directly inspired by [BizzoTech/docker-frappe](https://github.com/BizzoTech/docker-frappe) but derived adds an alpine variation, like provided by [donysukardi/docker-frappe](https://github.com/donysukardi/docker-frappe).
+
+:construction: **This image is still in development!**
+
+## What is Frappe ?
+
+Full-stack web application framework that uses Python and MariaDB on the server side and a tightly integrated client side library. Built for [ERPNext](https://erpnext.com/).
+
+> [frappe.io](https://frappe.io/)
+> [github frappe](https://github.com/frappe/frappe)
+
+## Supported tags
+
+https://hub.docker.com/r/monogramm/docker-frappe/
+
+* frappe 11.1
+    - `11.1-apache`, `11-apache`, `apache`, `11.1`, `latest`
+    - `11.1-fpm-alpine`, `fpm-alpine`
+    - `11.1-fpm`, `fpm`
+* frappe 10.1
+    - `10.1-apache`, `10-apache`
+    - `10.1-fpm-alpine`
+    - `10.1-fpm`
+
+# Questions / Issues
+If you got any questions or problems using the image, please visit our [Github Repository](https://github.com/Monogramm/docker-frappe) and write an issue.  
---
+++ update.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+set -eo pipefail
+
+declare -A base=(
+	[debian]='debian'
+	[alpine]='alpine'
+)
+
+variants=(
+	debian
+	alpine
+)
+
+
+# version_greater_or_equal A B returns whether A >= B
+function version_greater_or_equal() {
+	[[ "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1" || "$1" == "$2" ]];
+}
+
+min_versionFrappe=10.1
+
+dockerRepo="monogramm/docker-frappe"
+latestsFrappe=( $( curl -fsSL 'https://api.github.com/repos/frappe/frappe/tags' |tac|tac| \
+	grep -oE '[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+' | \
+	sort -urV ) 10.1.67 )
+
+latestsBench=( 4.1 master )
+
+# Remove existing images
+echo "reset docker images"
+find ./images -maxdepth 1 -type d -regextype sed -regex '\./images/[[:digit:]]\+\.[[:digit:]]\+' -exec rm -r '{}' \;
+
+echo "update docker images"
+travisEnv=
+for latest in "${latestsFrappe[@]}"; do
+	version=$(echo "$latest" | cut -d. -f1-2)
+
+	# Only add versions >= "$min_version"
+	if version_greater_or_equal "$version" "$min_versionFrappe"; then
+
+		for bench in "${latestsBench[@]}"; do
+
+			for variant in "${variants[@]}"; do
+				# Create the version+variant directory with a Dockerfile.
+				dir="images/frappe_$version-bench_$bench/$variant"
+				if [ -d "$dir" ]; then
+					continue
+				fi
+				echo "generating frappe $latest [$version] / bench $bench ($variant)"
+				mkdir -p "$dir"
+
+				template="Dockerfile-${base[$variant]}.template"
+				cp "$template" "$dir/Dockerfile"
+
+				# Replace the variables.
+				sed -ri -e '
+					s/%%VARIANT%%/'"$variant"'/g;
+					s/%%VERSION%%/'"$latest"'/g;
+					s/%%BRANCH%%/'"$bench"'/g;
+				' "$dir/Dockerfile"
+
+				# Copy the shell scripts
+				for name in entrypoint; do
+					cp "docker-$name.sh" "$dir/$name.sh"
+					chmod 755 "$dir/$name.sh"
+				done
+
+				cp ".dockerignore" "$dir/.dockerignore"
+
+				travisEnv='\n    - VERSION='"$version"' BENCH='"$bench"' VARIANT='"$variant$travisEnv"
+
+				if [[ $1 == 'build' ]]; then
+					tag="$version-$variant"
+					echo "Build Dockerfile for ${tag}"
+					docker build -t ${dockerRepo}:${tag} $dir
+				fi
+			done
+
+		done
+
+	fi
+
+done
+
+# update .travis.yml
+travis="$(awk -v 'RS=\n\n' '$1 == "env:" && $2 == "#" && $3 == "Environments" { $0 = "env: # Environments'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
+echo "$travis" > .travis.yml
