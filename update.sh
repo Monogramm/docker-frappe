e5734b0da75a75e023981a0392c72d5731fac2fe
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-08-05 14:56:53 +0200

    :fire: Remove private app install script
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- update.sh
+++ update.sh
@@ -72,7 +72,7 @@ for latest in "${latestsFrappe[@]}"; do
 				mkdir -p "$dir"
 
 				# Copy the shell scripts
-				for name in entrypoint.sh redis_cache.conf nginx.conf .env install_private_app.sh; do
+				for name in entrypoint.sh redis_cache.conf nginx.conf .env; do
 					cp "docker-$name" "$dir/$name"
 					chmod 755 "$dir/$name"
 					sed -i \
