729eebfb93efb385e3841c4f0ac5b5defb80b756
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-08-25 23:00:24 +0200

    :green_heart: Fix docket test exit codes
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- images/10.x-master/stretch-slim/docker_test.sh
+++ images/10.x-master/stretch-slim/docker_test.sh
@@ -2,27 +2,27 @@
 
 if [ ! -f "${FRAPPE_WD}/sites/apps.txt" ] || [ ! -f "${FRAPPE_WD}/sites/.docker-app-init" ]; then
     echo 'Apps were not installed in time!'
-    exit -1
+    exit 1
 fi
 
 if [ ! -f "${FRAPPE_WD}/sites/currentsite.txt" ] || [ ! -f "${FRAPPE_WD}/sites/.docker-site-init" ]; then
     echo 'Site was not installed in time!'
-    exit -2
+    exit 2
 fi
 
 if [ ! sudo ping -c 10 -q frappe_db ]; then
     echo 'Frappe database container is not responding!'
-    exit -4
+    exit 4
 fi
 
 if [ ! sudo ping -c 10 -q frappe_app ]; then
     echo 'Frappe app container is not responding!'
-    exit -8
+    exit 8
 fi
 
 if [ ! sudo ping -c 10 -q frappe_web ]; then
     echo 'Frappe web container is not responding!'
-    exit -16
+    exit 16
 fi
 
 # Success
