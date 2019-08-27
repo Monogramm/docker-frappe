9b3e783bbf1a7a4d5a0cc6a4f676ec1a748f8cf3
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-08-27 13:43:44 +0200

    :art: Set nginx server to localhost by default
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- update.sh
+++ update.sh
@@ -75,9 +75,6 @@ for latest in "${latestsFrappe[@]}"; do
 				for name in entrypoint.sh redis_cache.conf nginx.conf .env; do
 					cp "docker-$name" "$dir/$name"
 					chmod 755 "$dir/$name"
-					sed -i \
-						-e 's/{{ NGINX_SERVER_NAME }}/localhost/g' \
-					"$dir/$name"
 				done
 
 				case $frappe in
