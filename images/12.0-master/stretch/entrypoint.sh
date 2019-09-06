44a41cfab67e2144c226137514cfc38bc4829b3c
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-09-07 01:00:17 +0200

    :bug: Use root rights to write all log files
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- images/12.0-master/stretch/entrypoint.sh
+++ images/12.0-master/stretch/entrypoint.sh
@@ -124,8 +124,8 @@ bench_doctor() {
   setup_logs_owner
   log "Checking diagnostic info..."
   bench doctor \
-    | tee "${FRAPPE_WD}/logs/${NODE_TYPE}.log" 3>&1 1>&2 2>&3 \
-    | tee "${FRAPPE_WD}/logs/${NODE_TYPE}.err.log"
+    | sudo tee "${FRAPPE_WD}/logs/${NODE_TYPE}.log" 3>&1 1>&2 2>&3 \
+    | sudo tee "${FRAPPE_WD}/logs/${NODE_TYPE}.err.log"
 }
 
 bench_app() {
@@ -148,8 +148,8 @@ bench_app() {
   "${FRAPPE_WD}/env/bin/gunicorn" \
      $GUNICORN_ARGS \
     frappe.app:application --preload \
-    | tee "${FRAPPE_WD}/logs/${NODE_TYPE}.log" 3>&1 1>&2 2>&3 \
-    | tee "${FRAPPE_WD}/logs/${NODE_TYPE}.err.log"
+    | sudo tee "${FRAPPE_WD}/logs/${NODE_TYPE}.log" 3>&1 1>&2 2>&3 \
+    | sudo tee "${FRAPPE_WD}/logs/${NODE_TYPE}.err.log"
 }
 
 bench_build_apps() {
@@ -282,22 +282,22 @@ bench_migrate() {
 bench_scheduler() {
   log "Starting scheduler..."
   bench schedule \
-    | tee "${FRAPPE_WD}/logs/${NODE_TYPE}.log" 3>&1 1>&2 2>&3 \
-    | tee "${FRAPPE_WD}/logs/${NODE_TYPE}.err.log"
+    | sudo tee "${FRAPPE_WD}/logs/${NODE_TYPE}.log" 3>&1 1>&2 2>&3 \
+    | sudo tee "${FRAPPE_WD}/logs/${NODE_TYPE}.err.log"
 }
 
 bench_worker() {
   log "Starting $1 worker..."
   bench worker --queue "$1" \
-    | tee "${FRAPPE_WD}/logs/${NODE_TYPE}.log" 3>&1 1>&2 2>&3 \
-    | tee "${FRAPPE_WD}/logs/${NODE_TYPE}.err.log"
+    | sudo tee "${FRAPPE_WD}/logs/${NODE_TYPE}.log" 3>&1 1>&2 2>&3 \
+    | sudo tee "${FRAPPE_WD}/logs/${NODE_TYPE}.err.log"
 }
 
 bench_socketio() {
   log "Starting socketio..."
   node "${FRAPPE_WD}/apps/frappe/socketio.js" \
-    | tee "${FRAPPE_WD}/logs/${NODE_TYPE}.log" 3>&1 1>&2 2>&3 \
-    | tee "${FRAPPE_WD}/logs/${NODE_TYPE}.err.log"
+    | sudo tee "${FRAPPE_WD}/logs/${NODE_TYPE}.log" 3>&1 1>&2 2>&3 \
+    | sudo tee "${FRAPPE_WD}/logs/${NODE_TYPE}.err.log"
 }
 
 
