e98779ba38b931d173fa3f728fc510b4c415e147
mathieu.brunot <mathieu.brunot@monogramm.io>
2020-03-28 23:55:29 +0100

    ✅ Enable full frappe tests
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- images/12.2-master/slim-buster/test/docker_test.sh
+++ images/12.2-master/slim-buster/test/docker_test.sh
@@ -55,6 +55,10 @@ echo "Preparing Frappe application '${FRAPPE_APP_TO_TEST}' tests..."
 
 bench set-config allow_tests true
 
+bench doctor
+bench enable-scheduler
+bench doctor
+
 ################################################################################
 # TODO Frappe Unit tests
 # https://frappe.io/docs/user/en/guides/automated-testing/unit-testing
@@ -64,38 +68,50 @@ FRAPPE_APP_UNIT_TEST_PROFILE="$(pwd)/sites/.${FRAPPE_APP_TO_TEST}_unit_tests.pro
 
 #bench run-tests --help
 
-#echo "Executing Unit Tests of '${FRAPPE_APP_TO_TEST}' app..."
-#if [ "${TEST_VERSION}" = "10" ]; then
-#    bench run-tests \
-#        --app "${FRAPPE_APP_TO_TEST}" \
-#        --junit-xml-output "${FRAPPE_APP_UNIT_TEST_REPORT}" \
-#        --profile > "${FRAPPE_APP_UNIT_TEST_PROFILE}"
-#else
-#    bench run-tests \
-#        --app "${FRAPPE_APP_TO_TEST}" \
-#        --coverage \
-#        --junit-xml-output "${FRAPPE_APP_UNIT_TEST_REPORT}" \
-#        --profile > "${FRAPPE_APP_UNIT_TEST_PROFILE}"
-#fi
+echo "Executing Unit Tests of '${FRAPPE_APP_TO_TEST}' app..."
+if [ "${TEST_VERSION}" = "10" ]; then
+    bench run-tests \
+        --app "${FRAPPE_APP_TO_TEST}" \
+        --junit-xml-output "${FRAPPE_APP_UNIT_TEST_REPORT}" \
+        --profile > "${FRAPPE_APP_UNIT_TEST_PROFILE}"
+else
+    bench run-tests \
+        --app "${FRAPPE_APP_TO_TEST}" \
+        --coverage \
+        --junit-xml-output "${FRAPPE_APP_UNIT_TEST_REPORT}" \
+        --profile > "${FRAPPE_APP_UNIT_TEST_PROFILE}"
+fi
 
 ## Check result of tests
 if [ -f "${FRAPPE_APP_UNIT_TEST_REPORT}" ]; then
     echo "Checking Frappe application '${FRAPPE_APP_TO_TEST}' unit tests report..."
 
     if grep -E '(errors|failures)="[1-9][0-9]*"' "${FRAPPE_APP_UNIT_TEST_REPORT}"; then
-        echo "Unit Tests of '${FRAPPE_APP_TO_TEST}' app failed! See report for details:"
-        cat "${FRAPPE_APP_UNIT_TEST_REPORT}"
+        echo "Unit Tests of '${FRAPPE_APP_TO_TEST}' app failed! See logs for details."
+        #cat "${FRAPPE_APP_UNIT_TEST_REPORT}"
         exit 1
     else
-        echo "Unit Tests of '${FRAPPE_APP_TO_TEST}' app successful! See report for details:"
-        cat "${FRAPPE_APP_UNIT_TEST_REPORT}"
+        echo "Unit Tests of '${FRAPPE_APP_TO_TEST}' app successful!"
+        #cat "${FRAPPE_APP_UNIT_TEST_REPORT}"
     fi
 fi
 
 if [ -f ./sites/.coverage ]; then
-    echo "Sending Unit Tests coverage of '${FRAPPE_APP_TO_TEST}' app to Coveralls..."
     set +e
-    coveralls -b "$(pwd)/apps/${FRAPPE_APP_TO_TEST}" -d "$(pwd)/sites/.coverage"
+    cp ./sites/.coverage ./.coverage
+
+    echo "Unit Tests coverage report of '${FRAPPE_APP_TO_TEST}' app:"
+    coverage report -m
+
+    #echo "Sending Unit Tests coverage of '${FRAPPE_APP_TO_TEST}' app to Coveralls..."
+    #coveralls -b "$(pwd)/apps/${FRAPPE_APP_TO_TEST}" -d "$(pwd)/sites/.coverage"
+
+    # TODO When frappe supports coverage report in XML format
+    # https://github.com/frappe/frappe/issues/9696
+    #echo "Sending Unit Tests coverage of '${FRAPPE_APP_TO_TEST}' app to Codacy..."
+    #wget -qO - https://coverage.codacy.com/get.sh | sh -s report -l Python -r "$(pwd)/sites/coverage.xml"
+
+    rm ./.coverage
     set -e
 fi
 
@@ -111,7 +127,7 @@ fi
 # TODO QUnit (JS) Unit tests
 # https://frappe.io/docs/user/en/guides/automated-testing/qunit-testing
 
-#bench run-ui-tests --help
+bench run-ui-tests --help
 
 #echo "Executing UI Tests of '${FRAPPE_APP_TO_TEST}' app..."
 #if [ "${TEST_VERSION}" = "10" ] || [ "${TEST_VERSION}" = "11" ]; then
